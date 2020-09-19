d794a03f623196cbf17c1fe852da845688a40045
Valdir Junior <junior_vs@outlook.com>
2020-09-19 14:53:45 -0300

    cadastro de cupom

---
+++ spring/casadocodigo/src/main/java/org/ecorp/casadocodigo/repositories/CupomRepository.java
@@ -0,0 +1,8 @@
+package org.ecorp.casadocodigo.repositories;
+
+import org.ecorp.casadocodigo.model.Cupom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CupomRepository extends JpaRepository<Cupom, String> {
+
+}
